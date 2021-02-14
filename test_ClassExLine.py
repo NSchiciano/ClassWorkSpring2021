b787b47ee67278c1923e87cfb4a74923dd57ff3b
Nick Schiciano <NickS@Nicks-MBP.lan>
2021-02-14 18:45:09 -0500

    Second Testing Function

--- test_ClassExLine.py
+++ test_ClassExLine.py
@@ -4,8 +4,18 @@ import pytest
 @pytest.mark.parametrize("[a,b], [c,d], expected", [
     ([1,2], [3,4], 1),
     ([-1,-2], [-3,-4], 1),
-    ([1,1], [2,1], 0),
+    ([1.5,1.5], [2.5,1.5], 0),
 ])
 def test_calc_slope([a,b], [c,d], expected):
+    answer = calc_slope([a,b], [c,d])
+    assert answer == expected
+
+
+@pytest.mark.parametrize("[a,b], m, expected", [
+    ([1,2], 1, 1),
+    ([-1,-2], 1, -1),
+    ([1.5,1.5], 0, 1.5),
+])
+def test_calc_intercept([a,b], slope, expected):
     answer = calc_slope([a,b], [c,d])
     assert answer == expected
