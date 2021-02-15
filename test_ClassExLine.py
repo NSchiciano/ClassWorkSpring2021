b1b9d0711dda84728ac48c9ee073e13c29ba8ef6
Nick Schiciano <NickS@Nicks-MBP.lan>
2021-02-14 21:13:19 -0500

    Fixed errors in unit testing by actually importing the functions

--- test_ClassExLine.py
+++ test_ClassExLine.py
@@ -1,21 +1,23 @@
 # test_ClassExLine.py
 import pytest
 
-@pytest.mark.parametrize("[a,b], [c,d], expected", [
+@pytest.mark.parametrize("a, b, expected", [
     ([1,2], [3,4], 1),
     ([-1,-2], [-3,-4], 1),
     ([1.5,1.5], [2.5,1.5], 0),
 ])
-def test_calc_slope([a,b], [c,d], expected):
-    answer = calc_slope([a,b], [c,d])
+def test_calc_slope(a, b, expected):
+    from ClassExLine import calc_slope
+    answer = calc_slope(a, b)
     assert answer == expected
 
 
-@pytest.mark.parametrize("[a,b], m, expected", [
+@pytest.mark.parametrize("a, slope, expected", [
     ([1,2], 1, 1),
     ([-1,-2], 1, -1),
     ([1.5,1.5], 0, 1.5),
 ])
-def test_calc_intercept([a,b], slope, expected):
-    answer = calc_slope([a,b], [c,d])
+def test_calc_intercept(a, slope, expected):
+    from ClassExLine import calc_intercept
+    answer = calc_intercept(a, slope)
     assert answer == expected
